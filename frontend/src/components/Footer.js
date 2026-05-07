adb10f414be0c22b3c8ed76c06a11c4c15df80d5
meta649 <sk7277381428@gmail.com>
2026-05-07 07:27:32 +0530

    Add Footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2026 SSM Panel. All rights reserved.</p>
+        <p>Professional Social Media Marketing Services</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
